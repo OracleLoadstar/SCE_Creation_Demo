8732ed48942cf51c28ff1cd51d48f448b3288978
xingguangcuican6666 <xingguangcuican666@foxmail.com>
2025-04-12 12:16:50 +0800

    -v

---
+++ push.bat
@@ -0,0 +1,14 @@
+@echo off
+
+:main
+powershell cat ./version.txt
+call :fetch_version
+echo %version_name% > .\version.txt
+echo %version_name% > .\version_info\last_version.txt
+git add . -v
+git commit -m %commit% -v
+git push -v
+goto eof
+:fetch_version
+set /p version_name="请提供版本号："
+set /p commit="请提供commit："
